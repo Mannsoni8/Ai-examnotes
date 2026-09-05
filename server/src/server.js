44b21ba6ae4254c1bd86934e7f4709f487dd21a4
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-05 13:32:41 +0530

    feat(server): enhance server setup with DNS configuration and code formatting

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import app from "./app/app.js";
 import config from "./config/config.js";
 import { connectDB } from "./config/db.js";
 
-await connectDB()
-const port = config.PORT
+await connectDB();
+const port = config.PORT;
 
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
