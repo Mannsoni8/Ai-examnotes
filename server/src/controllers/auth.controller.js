8bcd440ea7d1086ea2c1ea9df07a116d45b11e17
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 12:12:31 +0530

    fix(auth): improve error handling and response structure in googleAuthController

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -5,9 +5,21 @@ export const googleAuthController = async (req, res) => {
   try {
     const { name, email } = req.body;
     let user = await userModel.findOne({ email });
-    if(!user){
-        user = await userModel.create({name,email})
+    if (!user) {
+      user = await userModel.create({ name, email });
     }
-    const token = await getToken(user._id)
-  } catch (error) {}
+    let token = await getToken(user._id);
+    res.cookies("token", token, {
+      httpOnly: true,
+      secure: false,
+      samesite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({
+        message:"User is register",
+        user
+    })
+  } catch (error) {
+    return res.status(400).json({message:`Error in google siguo ${error}`})
+  }
 };
