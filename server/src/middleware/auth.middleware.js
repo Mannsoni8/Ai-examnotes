2a144c5bdb0eae736b9320780e0fb826c583b686
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 14:38:42 +0530

    feat(auth): implement authentication middleware for token verification

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import config from "../config/config.js";
+import { getToken } from "../utils/token";
+import jwt from "jsonwebtoken";
+export const authMiddleware = async (req, res, next) => {
+  try {
+    let { token } = req.cookies;
+    if (!token) {
+      return res.status(400).json({
+        messahe: "Token is not found",
+      });
+    }
+    let verifyToken = jwt.verify(token, config.JWT_SECRET);
+    if (!verifyToken) {
+      return res.status(400).json({
+        messahe: "User does not have valid token",
+      });
+    }
+    req.userId = verifyToken.userId;
+    next();
+  } catch (error) {
+     return res.status(500).json({
+        messahe: `Erro in auth ${error}`,
+      });
+  }
+};
