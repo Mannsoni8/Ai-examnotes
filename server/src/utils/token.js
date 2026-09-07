d116ae8abb2165813874dcd20c409508aa1511c0
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 12:00:34 +0530

    fix(token): return token and log error in getToken function

--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -4,5 +4,8 @@ import config from "../config/config.js";
 export const getToken = async (userId) => {
   try {
     const token = jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: "7d" });
-  } catch (error) {}
+    return token
+  } catch (error) {
+    console.log("Error in geting token",error)
+  }
 };
