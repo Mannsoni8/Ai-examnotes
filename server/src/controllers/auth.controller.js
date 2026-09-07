6600b521321bb607497d78f11e94f232b6a6ec9b
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 12:12:45 +0530

    fix(auth): format response messages and improve error handling in googleAuthController

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -16,10 +16,10 @@ export const googleAuthController = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json({
-        message:"User is register",
-        user
-    })
+      message: "User is register",
+      user,
+    });
   } catch (error) {
-    return res.status(400).json({message:`Error in google siguo ${error}`})
+    return res.status(400).json({ message: `Error in google siguo ${error}` });
   }
 };
