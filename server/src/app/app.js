33e57ff5786dd565aed29435fd02f8c8548f96a8
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 12:22:55 +0530

    fix: correct CORS configuration to use CLIEN_URL from config

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -2,13 +2,14 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import router from "../routes/auth.route.js";
 import cors from "cors"
+import config from "../config/config.js";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    origin:
-))
+app.use(cors({
+    origin: config.CLIEN_URL,
+}));
 
 app.get("/", (res, req) => {
   res.send("backend is running");
