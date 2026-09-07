905c5815787a10ad92e856eb1ddf72f11ae9ddb3
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 12:48:42 +0530

    fix: correct formatting of CORS middleware configuration in app.js

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import router from "../routes/auth.route.js";
-import cors from "cors"
+import cors from "cors";
 import config from "../config/config.js";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: config.CLIEN_URL,
-}));
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "OPTIONS"],
+  }),
+);
 
 app.get("/", (res, req) => {
   res.send("backend is running");
