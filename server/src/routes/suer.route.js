473d1be22ae29d2324c6e500fcfb1380283fbbc1
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 15:19:25 +0530

    feat(routes): add getUserController to curr-user route for user details retrieval

--- server/src/routes/suer.route.js
+++ server/src/routes/suer.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
+import { getUserController } from "../controllers/user.controller";
 
 const useRouter = Router();
 
-useRouter.get("/curr-user", authMiddleware);
+useRouter.get("/curr-user", authMiddleware, getUserController);
 
 export default useRouter;
