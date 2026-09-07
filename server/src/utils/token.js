f9953edbe084d755b7e99c9a7ce412644b7ab9dc
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-07 11:59:48 +0530

    feat: add auth controller, route, and token utility for JWT handling

---
+++ server/src/utils/token.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
+export const getToken = async (userId) => {
+  try {
+    const token = jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: "7d" });
+  } catch (error) {}
+};
