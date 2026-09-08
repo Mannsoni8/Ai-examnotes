ef7240b26a828643e268985dff14f47519a102bb
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-08 16:16:41 +0530

    feat(navbar): enhance Navbar component with styling and layout improvements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div><h1>Navbar</h1></div>
-  )
-}
+    <div
+      className="relative z-20 mx-6 mt-6
+       rounded-2xl
+       bg-gradient-to-br from-black/90 via-black/80 to-black/90 backdrop-blur-2xl 
+       border border-white/10
+       shadow-[0_22px_55px_rgba(0,0,0,0.75)]
+       flex items-center justify-between px-8 py-4">
+      <h1>Navbar</h1>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
