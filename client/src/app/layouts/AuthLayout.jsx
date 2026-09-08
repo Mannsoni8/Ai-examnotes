3116510b9524166dc9fd0cd078c7b714e0e9d9d9
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-08 13:44:41 +0530

    feat(layouts): add userData selection from Redux store in AuthLayout

--- client/src/app/layouts/AuthLayout.jsx
+++ client/src/app/layouts/AuthLayout.jsx
@@ -2,13 +2,17 @@ import { Outlet } from "react-router";
 import Auth from "../../pages/Auth";
 import { useEffect } from "react";
 import { getCurrentUser } from "../../services/api";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const AuthLayout = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     getCurrentUser(dispatch);
   }, [dispatch]);
+
+  const { userData } = useSelector((state) => state.user);
+
   return (
     <div>
       <div>
