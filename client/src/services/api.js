766a59544462bdb6ec19fcfad80aa0546d30c226
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-08 13:31:55 +0530

    feat(api): integrate setUserData dispatch in getCurrentUser function

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,11 @@
 import api from "../api/axiosinstance";
+import { setUserData } from "../redux/userSlice";
 
 export const getCurrentUser = async (dispatch) => {
   try {
     const re = await api.get("/curr-user");
     console.log(re.data);
+    dispatch(setUserData(re.data));
   } catch (error) {
     console.log("errorn in getting current user", error);
   }
