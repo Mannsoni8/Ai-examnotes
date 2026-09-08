9f73ea75d920c80f9115d18a33f439f065c10e9f
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-08 16:11:01 +0530

    feat(navbar): add Navbar component and integrate it into Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen overflow-hidden bg-white text-black">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
