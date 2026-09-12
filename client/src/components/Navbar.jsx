ae932abfe77c286bde057a623ab439bbbe36dc03
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-12 22:11:14 +0530

    feat(navbar): add animated credits display in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import logo from "../assets/logo.png";
 import { useSelector } from "react-redux";
 import { useState } from "react";
@@ -47,6 +47,20 @@ const Navbar = () => {
               ➕
             </motion.span>
           </motion.div>
+          {showCredits && (
+            <AnimatePresence>
+              {" "}
+              <motion.div
+                initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 10, scale: 1 }}
+                exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                transition={{ duration: 0.2 }}
+                className="absolute right-0 mt-4 w-64 rounded-2xl
+              bg-black/50 backdrop-blur-xl
+              border border-white/10
+              shadow-[0_25px_60px_rgba(0,0,0,0.7)] p-4 text-white"></motion.div>
+            </AnimatePresence>
+          )}
         </div>
       </div>
     </motion.div>
