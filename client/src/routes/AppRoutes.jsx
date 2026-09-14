3c646441737fa8745dfd88b83a964360cc7531b4
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-14 17:41:10 +0530

    feat(routes): add new routes for Notes, History, and Pricing pages

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import AuthLayout from "../app/layouts/AuthLayout";
 import MainLayout from "../app/layouts/MainLayout";
 import Home from "../pages/Home";
+import Notes from "../pages/Notes";
+import Pricing from "../pages/Pricing";
 
 const AppRoutes = () => {
   const router = createBrowserRouter([
@@ -16,6 +18,18 @@ const AppRoutes = () => {
         {
           path: "",
           element: <Home />,
+        },
+        {
+          path: "notes",
+          element: <Notes />,
+        },
+        {
+          path: "history",
+          element: <History />,
+        },
+        {
+          path: "priceing",
+          element: <Pricing />,
         },
       ],
     },
